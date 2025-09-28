a1463168e3b8a9d8a61ef7e273a299827def4c5e
Karen Johnstron <228061156+KE-Johnston1@users.noreply.github.com>
2025-09-28 14:41:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("✅ main.py is running!")
+import subprocess
+
+def run_module():
+    print("[+] Running correlate_logs.py...")
+    subprocess.run(["python", "parsers/correlate_logs.py"])
+
+if __name__ == "__main__":
+    run_module()
+
